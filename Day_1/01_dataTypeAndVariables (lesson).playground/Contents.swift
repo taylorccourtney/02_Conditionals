7b28f2a709d7cbfee41d6ba0669402f609ae3210
Apple <apple@Apples-MacBook-Pro-45.local>
2019-07-22 15:41:11 -0400

    inital commit

---
+++ Day_1/01_dataTypeAndVariables (lesson).playground/Contents.swift
@@ -0,0 +1,21 @@
+/*var first="Karlie"
+var last="Kloss"
+"\(first)\(last)"
+"\(last)\(first)"
+"\(first) \(last)"
+"\(last) \(first) \(last) \(first)"
+"I love \(first)"
+*/
+
+/*var a = 12
+var b = 65
+var c = 31
+var d = 98
+var average = (a + b + c + d)/4
+print (average)
+*/
+
+let pay = 10.25
+print(pay * 20)
+
+
